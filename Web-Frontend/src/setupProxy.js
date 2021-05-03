cb43981307ce02318127799cc36ddfe7961257db
kkimbj18 <kkimg3802@gmail.com>
2021-05-04 06:24:10 +0900

    a

--- Web-Frontend/src/setupProxy.js
+++ Web-Frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://cabstone.ml',
+      target: 'http://cabstone.ml',
       changeOrigin: true
     })
   );
