f4de79944b8d59b6343588e6fd318f8f7e373cb2
KimSuMin <thp04123@ajou.ac.kr>
2021-05-24 09:30:09 +0900

    main page

--- Web-Frontend/src/components/views/MainPage/Index.js
+++ Web-Frontend/src/components/views/MainPage/Index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Calendar } from 'antd';
 import 'antd/dist/antd.css';
+import logo from '../../../images/utils/logo_main.png'
+import logoT from '../../../images/utils/logoT_main.png'
+import back from '../../../images/utils/back_main.png'
 
 import styled from 'styled-components';
 
@@ -27,19 +30,45 @@ color : #8b8b8b;
 font-size : 13px;
 font-weight: 400;
 `
-
+const Btn = styled.button`
+width : 250px;
+height : 50px;
+border : none;
+font-size : 24px;
+font-weight : bold;
+background-color: #407AD6;
+color : white;
+text-align :center;
+line-height : 40px;
+border-radius : 40px;
+margin-top : 50px;
+`
 function Index() {
     return (
         <div>
             <Container style={{marginLeft: "20px", marginTop: '10px'}}>
-                    <Title>Main page</Title>
+                    <Title>Main Page</Title>
                     <div style={{width: "100%", display: "block"}}>
-                        <SubTitle>Main page</SubTitle>
+                        <SubTitle>메인 페이지</SubTitle>
                     </div>
                     <hr style={{width: "100%", margin: "30px 0px", marginTop: "50px",display:"block", borderColor: '#ffffff'}}/>
-                    <div style={{textAlign:'center', marginTop:'300px', fontSize:'30px', fontStyle:'italic'}}>
-                        Default Page
-                    </div>                    
+            
+                    <div style={{width: "42%", float: "left", marginTop: "60px", marginLeft: "30px"}}>
+                        <div style={{width: "500px"}}> 
+                            <img style = {{float: "left", width: "60px"}} src= {logo} alt="logo"/>
+                            <img style = {{float: "left", maxWidth: "100%", height: "55px", marginLeft: "10px", marginTop: "5px"}} src= {logoT} alt="logoT"/>
+                        </div>
+                        <div style = {{marginTop: "150px", color: "#444444", fontSize: "37px", fontWeight: "bold", fontFamily: "Verdana"}}>ONLINE</div>         
+                        <div style = {{marginBottom: "50px", color: "#444444", fontSize: "37px", fontWeight: "bold", fontFamily: "Verdana"}}>COURSES SERVICE</div>                  
+                        <div style = {{fontSize: "23px", color: "#b5b5b5", fontFamily: "Verdana", lineHeight: '45px', fontWeight: "bold",}}>Experience a new class with real-time lecture
+                            service disboard! Instructors and students can 
+                            have even more amazing experiences in class.
+                            Also, there are many out-of-class services as well.</div>
+                        <Btn onClick = {{}}>LEARN MORE</Btn>
+                    </div>     
+                    <div style={{float: "left", width: "870px", height: "690px"}}>  
+                        <img style = {{marginTop: "20px", maxWidth: "100%", height: "100%"}} src= {back} alt="back"/>
+                    </div>
                     </Container>
         </div>
     )
